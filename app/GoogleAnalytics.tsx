0a4e419b0d0905d53af0e1ae7d9155f8238c84bd
satohidetada <karatedamashi.123@gmail.comkarateshin.123@gmail.com>
2026-08-13 08:15:36 +0900

    add GA4, sitemap and robots.txt

---
+++ app/GoogleAnalytics.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import Script from 'next/script'
+
+const GA_ID = 'G-PT5KD6NQDJ'
+
+export default function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="ga4-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}');
+        `}
+      </Script>
+    </>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AdMax from "./AdMax";
+import GoogleAnalytics from "./GoogleAnalytics";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <GoogleAnalytics />
         {children}
         <AdMax />
         <footer
